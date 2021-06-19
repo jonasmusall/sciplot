390538fb3511633694f9446aa21aea140c959752
jonasmusall <jonas.musall@gmail.com>
2021-06-19 19:36:23 +0200

    Improve code readability

--- SciPlot.m
+++ SciPlot.m
@@ -3,7 +3,9 @@
 BeginPackage["SciPlot`"]
 SciPlot::usage = "SciPlot[{{\!\(\*SubscriptBox[\(f\), \(1\)]\),\!\(\*SubscriptBox[\(f\), \(2\)]\),...},{x,\!\(\*SubscriptBox[\(x\), \(min\)]\),\!\(\*SubscriptBox[\(x\), \(max\)]\)}},{{\!\(\*SubscriptBox[\(x\), \(1\)]\),\!\(\*SubscriptBox[\(y\), \(1\)]\)},{\!\(\*SubscriptBox[\(x\), \(2\)]\),\!\(\*SubscriptBox[\(y\), \(2\)]\)},...},...] plots all specified functions and lists"
 Begin["`Private`"]
+
 InternalPlotMarkers = {{Graphics[{Thin, Line[{{{-1, -1}, {1, 1}}, {{-1, 1}, {1, -1}}}]}], Medium}}
+
 Options[InternalSinglePlot] = {
     PlotRange -> Automatic,
     Axes -> False,
@@ -11,12 +13,25 @@ Options[InternalSinglePlot] = {
     PlotMarkers -> InternalPlotMarkers[[1]],
     PerformanceGoal -> "Quality",
     MaxPlotPoints -> Infinity,
-    MaxRecursion -> Automatic}
-Options[SciPlot] = {PlotRange -> Automatic}
+    MaxRecursion -> Automatic
+}
+Options[SciPlot] = {
+    PlotRange -> Automatic
+}
+
 InternalSinglePlot[{f_, {x_Symbol, xmin_?NumericQ, xmax_?NumericQ}}, OptionsPattern[]] :=
-    Plot[f, {x, xmin, xmax}, PlotRange -> OptionValue[PlotRange], PerformanceGoal -> OptionValue[PerformanceGoal], MaxRecursion -> OptionValue[MaxRecursion]]
+    Plot[f, {x, xmin, xmax},
+        PlotRange -> OptionValue[PlotRange],
+        PerformanceGoal -> OptionValue[PerformanceGoal],
+        MaxRecursion -> OptionValue[MaxRecursion]
+    ]
 InternalSinglePlot[list_List, OptionsPattern[]] :=
-    ListPlot[list, PlotRange -> OptionValue[PlotRange], PerformanceGoal -> OptionValue[PerformanceGoal], MaxPlotPoints -> OptionValue[MaxPlotPoints]]
+    ListPlot[list,
+        PlotRange -> OptionValue[PlotRange],
+        PerformanceGoal -> OptionValue[PerformanceGoal],
+        MaxPlotPoints -> OptionValue[MaxPlotPoints]
+    ]
+
 SciPlot[x__?(Or[Head[#] == List, MatchQ[#, {_, {_Symbol, _?NumericQ, _?NumericQ}}]]&), OptionsPattern[]] :=
     Module[{plotRange = OptionValue[PlotRange]},
         If[plotRange == Automatic,
@@ -28,5 +43,6 @@ SciPlot[x__?(Or[Head[#] == List, MatchQ[#, {_, {_Symbol, _?NumericQ, _?NumericQ}
             TicksStyle -> Directive[Black, FontSize -> 12, FontFamily -> "Times"]
         ]
     ]
+
 End[]
 EndPackage[]
